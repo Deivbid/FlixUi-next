6ac5b6a0df1b799b0c8b15615526c2a6428a199d
David Aparicio <davidaapariciobak@gmail.com>
2023-05-09 15:12:20 -0300

    Added authentication and auth view

---
+++ pages/auth.tsx
@@ -0,0 +1,123 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client';
+import Input from '@/components/Input';
+import axios from 'axios';
+import React, { useCallback, useState } from 'react';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
+
+enum AccountVariant {
+  LOGIN = 'LOGIN',
+  REGISTER = 'REGISTER',
+}
+
+const Auth = () => {
+  const router = useRouter();
+
+  const [email, setEmail] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const [variant, setVariant] = useState<AccountVariant>(AccountVariant.LOGIN);
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant: AccountVariant) =>
+      currentVariant === AccountVariant.LOGIN
+        ? AccountVariant.REGISTER
+        : AccountVariant.LOGIN
+    );
+  }, []);
+
+  const login = useCallback(async () => {
+    console.log('Hola papus!');
+    try {
+      await signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: '/',
+      });
+
+      router.push('/');
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    console.log('variant ', variant);
+    try {
+      await axios.post('/api/register', {
+        email,
+        name,
+        password,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password]);
+
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" className="h-12" alt="netflix-logo" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === AccountVariant.LOGIN ? 'Sign in' : 'Register'}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === AccountVariant.REGISTER && (
+                <Input
+                  label="Username"
+                  onChange={(ev: any) => setName(ev.target.value)}
+                  id="user"
+                  value={name}
+                />
+              )}
+              <Input
+                label="Email"
+                onChange={(ev: any) => setEmail(ev.target.value)}
+                id="email"
+                type="email"
+                value={email}
+              />
+
+              <Input
+                label="Password"
+                onChange={(ev: any) => setPassword(ev.target.value)}
+                id="password"
+                type="password"
+                value={password}
+              />
+            </div>
+
+            <button
+              onClick={variant === AccountVariant.LOGIN ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
+              {variant === AccountVariant.LOGIN ? 'Login' : 'Sign up'}
+            </button>
+            <p className="text-neutral-500 mt-12">
+              {variant === AccountVariant.LOGIN
+                ? 'Fist time using netflix?'
+                : 'Already have an account'}
+              <span
+                className="text-white ml-1 hover:underline cursor-pointer"
+                onClick={toggleVariant}
+              >
+                {variant === AccountVariant.LOGIN
+                  ? 'Create an account'
+                  : 'Login'}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
