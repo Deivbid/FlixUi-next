d12f4f405613c3cba3a48d450b3947e69cb98451
David Aparicio <davidaapariciobak@gmail.com>
2023-05-10 16:00:05 -0300

    building homepage UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
 import Navbar from '@/components/Navbar';
+import useMovieList from '@/hooks/useMovieList';
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
+
   if (!session) {
     return {
       redirect: {
@@ -20,10 +23,14 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Home = () => {
+  const { data: movies } = useMovieList();
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title={'Trending Now'} data={movies} />
+      </div>
     </>
   );
 };
