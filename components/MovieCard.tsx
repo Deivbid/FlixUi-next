17f6e5cdb7fd317517ec9aaebe02a167c7458565
David Aparicio <davidaapariciobak@gmail.com>
2023-05-10 18:06:07 -0300

    added watch route with video player

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Movie } from './MovieList';
 import { BsPlayFill } from 'react-icons/bs';
 import FavoriteButton from './FavoriteButton';
+import { useRouter } from 'next/router';
 
 interface MovieCardProps {
   movie: Movie;
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
+  const router = useRouter();
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
       <img
@@ -23,7 +25,10 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
         />
         <div className="z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md">
           <div className="flex flex-row items-center gap-3">
-            <div className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300">
+            <div
+              className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300"
+              onClick={() => router.push(`/watch/${movie?.id}`)}
+            >
               <BsPlayFill size={30} />
             </div>
             <FavoriteButton movieId={movie?.id} />
