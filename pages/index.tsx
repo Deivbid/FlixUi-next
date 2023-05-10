e2f5dac89f25b1eb10907b341146edc84f1a5912
David Aparicio <davidaapariciobak@gmail.com>
2023-05-10 19:57:18 -0300

    building modal preview and final modifications

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import Billboard from '@/components/Billboard';
+import InfoModal from '@/components/InfoModal';
 import MovieList from '@/components/MovieList';
 import Navbar from '@/components/Navbar';
 import useFavoriteMovies from '@/hooks/useFavoritesMovies';
+import useInfoModal from '@/hooks/useInfoModal';
 import useMovieList from '@/hooks/useMovieList';
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
@@ -26,8 +28,10 @@ export async function getServerSideProps(context: NextPageContext) {
 const Home = () => {
   const { data: movies } = useMovieList();
   const { data: favoriteMoviews } = useFavoriteMovies();
+  const { isOpen, closeModal } = useInfoModal();
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
       <div className="pb-40">
